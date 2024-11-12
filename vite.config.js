5dcc2753b7a79ee50c137bc5ad9095528c2ab155
malcomowilla <owillamalcom@gmail.com>
2024-11-12 20:33:22 +0300

    request headers

--- vite.config.js
+++ vite.config.js
@@ -17,11 +17,14 @@ export default defineConfig({
     },
     proxy: {
       '/api': {
-       
         target: 'https://aitechs-sas-garbage-solution-backend.onrender.com',
-        // target: 'http://localhost:4000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
+        configure: (proxy) => {
+          proxy.on('proxyReq', (proxyReq, req, res, options) => {
+            proxyReq.setHeader('X-Original-Host', req.headers.host);
+          });
+        },
       },
     },
   },
