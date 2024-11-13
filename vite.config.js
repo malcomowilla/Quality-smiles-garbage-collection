15f767c8948d3d253c9446f9fea48d93cecfbadc
malcomowilla <owillamalcom@gmail.com>
2024-11-13 18:08:25 +0300

    correcting admin error

--- vite.config.js
+++ vite.config.js
@@ -18,8 +18,8 @@ export default defineConfig({
     
     proxy: {
       '/api': {
-        // target: 'https://aitechs-sas-garbage-solution-backend.onrender.com',
-        target: 'http://localhost:4000',
+        target: 'https://aitechs-sas-garbage-solution-backend.onrender.com',
+        // target: 'http://localhost:4000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
         configure: (proxy) => {
